9ca537f9006348de8b24678c3ebd89144aeabf78
Allan Santos Faria <santosfariaallan025@gmail.com>
2026-08-06 17:01:12 -0300

    acabo css

---
+++ AULA/Minha-loja/js/script.js
@@ -0,0 +1,36 @@
+const nomeLoja = "MULTI CONSOLE";
+
+let produto1 = "Playstation 5"
+let preco1 = 3.704;
+let emPromocao = true;
+
+function calcularDesconto(preco, desconto) {
+    return preco - (preco * desconto);
+}
+
+let precoFinal;
+
+if (emPromocao) {
+    precoFinal = calcularDesconto(preco1, 0.1);
+} else {
+    precoFinal = preco1;
+}
+
+console.log(`Produto: ${produto1}`);
+console.log(`Preço final: R$ ${precoFinal}`);
+
+const produtos = [
+    { nome: "Playstation 5", preco: 3704 },
+    { nome: "Xbox Series", preco: 3704 },
+    { nome: "Nitendo Switch", preco: 3704 }
+];
+
+for (let i = 0; i < produtos.length; i++) {
+    console.log(`Produto: ${produtos[i].nome} - R$ ${produtos[i].preco}`);
+}
+
+const exibirProduto = (produto) => {
+    return `Produto: ${produto.nome} - R$ ${produto.preco}`;
+};
+
+console.log(exibirProduto(produtos[0]));
